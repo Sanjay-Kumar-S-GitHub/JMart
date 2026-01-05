4162febcfec3e6daeca5fd61debda8efbd8be6d2
Sanjay-Kumar-S-GitHub <sanjay.k.sundararajan@gmail.com>
2026-01-05 21:14:47 +0530

    Projec started

---
+++ src/com/jmart/main/Main.java
@@ -0,0 +1,9 @@
+package com.jmart.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to JMart");
+
+        System.out.println("Thank you for shopping with JMart!");
+    }
+}
---
+++ src/com/jmart/services/ProductService.java
@@ -0,0 +1,4 @@
+package com.jmart.services;
+
+public class ProductService {
+}
